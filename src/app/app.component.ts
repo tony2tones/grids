985a476925b0645442e8ef13c1af09da660a3a64
Anthony <anthonymarques@outlook.com>
2020-06-06 01:31:23 +0200

    added basic styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./base.css', './basic-grid.css']
 })
 export class AppComponent {
   title = 'theGrid';
